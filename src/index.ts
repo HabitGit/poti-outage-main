8fa9fd8e4097c747359005541eb9cb0dc5082442
Igor <char.habit@gmail.com>
2023-07-05 13:11:34 +0400

    fix linter

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import {MainController} from "./controllers/main.controller";
 import TelegramBot from "node-telegram-bot-api";
 import {AppDataSource} from "./db/data-source";
-import {commands} from "./commands/commands";
+// import {commands} from "./commands/commands";
 import {CronJob} from "cron";
 import {WaterService} from "./service/water.service";
 import {ElectricityService} from "./service/electricity.service";
