b771d2f652db051a17316ab42a82084b3021e809
Igor <char.habit@gmail.com>
2023-07-19 21:32:53 +0400

    refactoring street service done

--- src/service/streets.service.ts
+++ src/service/streets.service.ts
@@ -1,22 +1,22 @@
 import TelegramBot from 'node-telegram-bot-api';
-import { bot } from '../index';
 import { StreetsRepository } from '../db/repository/streets.repository';
 import { UsersRepository } from '../db/repository/users.repository';
-import { UpdateUserDto } from '../templates/update-user.dto';
-import { Helper } from './helper';
-import { IGetUserPoints } from '../templates/interfaces';
-import { CreateStreetDto } from '../templates/create-street.dto';
+import { Helper } from '../templates/helpers/helper';
+import { IGetUserPoints } from '../templates/interfaces/interfaces';
+import { BotService } from './bot.service';
 import { Streets } from '../db/entitys/streets.entity';
+import { UpdateUserDto } from '../templates/dtos/update-user.dto';
+import { CreateStreetDto } from '../templates/dtos/create-street.dto';
 
 export class StreetsService {
   constructor(
     private streetsRepository: StreetsRepository,
     private usersRepository: UsersRepository,
     private helper: Helper,
+    private botService: BotService,
   ) {}
 
   registrationStreet = async (msg: TelegramBot.Message) => {
-    console.log('RegStr work');
     const {
       message: streetName,
       chatId,
@@ -24,37 +24,43 @@ export class StreetsService {
     }: IGetUserPoints = this.helper.getUserPoints(msg);
 
     if (!userId) {
-      return bot.sendMessage(chatId, 'Нету данных о вашем телеграмм аккаунте');
+      return this.botService.sendMessage(
+        chatId,
+        'Нету данных о вашем телеграмм аккаунте',
+      );
     }
 
     if (!streetName) {
-      return bot.sendMessage(chatId, 'Вы не указали улицу, попробуйте снова');
+      return this.botService.sendMessage(
+        chatId,
+        'Вы не указали улицу, попробуйте снова',
+      );
     }
 
-    const isStreet = await this.streetsRepository.getStreetByNameGeo(
-      streetName,
-    );
+    const isStreet: Streets | null =
+      await this.streetsRepository.getStreetByNameGeo(streetName);
 
     if (!isStreet) {
-      return bot.sendMessage(
+      await this.botService.messageListenerOff(this.registrationStreet);
+      return this.botService.sendMessage(
         chatId,
-        'Такой улица пока что не добавлена в базу, или указана с ошибкой',
+        'Такая улица пока что не добавлена в базу, или указана с ошибкой',
       );
     }
     const updateUserData: UpdateUserDto = { userId: userId, street: isStreet };
     await this.usersRepository.updateUserByUserId(updateUserData);
-    await bot.sendMessage(chatId, 'Улица успешно зарегистрирована');
-    await bot.removeListener('message', this.registrationStreet);
+    await this.botService.sendMessage(chatId, 'Улица успешно зарегистрирована');
+    await this.botService.messageListenerOff(this.registrationStreet);
   };
 
   async addStreets(streetsData: CreateStreetDto[]): Promise<Streets[]> {
     const createdStreets: Streets[] = [];
-    await streetsData.forEach(async (streetData) => {
+    for (const streetData of streetsData) {
       const street: Streets = await this.streetsRepository.createStreet(
         streetData,
       );
       createdStreets.push(street);
-    });
+    }
     return createdStreets;
   }
 }
