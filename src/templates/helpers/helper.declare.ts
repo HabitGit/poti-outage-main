c6200aa0bf5d619c3d429cc5a871b01420cb160f
Igor <char.habit@gmail.com>
2023-07-28 21:52:27 +0400

    add declare class to helper

---
+++ src/templates/helpers/helper.declare.ts
@@ -0,0 +1,25 @@
+import {
+  IFinishParserInfoObject,
+  IGetUserPoints,
+} from '../interfaces/interfaces';
+import TelegramBot from 'node-telegram-bot-api';
+
+export declare class HelperDeclare {
+  /**
+   * Принимает объект спаршеных данных и преобразует в строку.
+   * @Param typeOfPublicService Название аварийной инфраструктуры типа ```string```
+   * @Param info Объект спаршенных данных типа ```IFinishParserInfoObject``` среди которых дата начала отключения типа ```Date```, дата окончания отключения типа ```Date```, массив улиц на которых произойдет отключение типа ```string[]```.
+   * @Return { Найдены отключения { воды \ электричества } с { Дата начала } по { Дата окончания } На улицах: { Перечисление улиц }}
+   */
+  infoOutputRefactoring(
+    typeOfPublicService: string,
+    info: IFinishParserInfoObject,
+  ): string;
+
+  /**
+   * Получение нужных значений из сообщения боту.
+   * @Param msg - сообщение боту типа ```Message``` класса ```TelegramBot```.
+   * @Return {  chatId: number; userId: number | undefined; userName: string | undefined; message: string | undefined; }
+   * */
+  getUserPoints(msg: TelegramBot.Message): IGetUserPoints;
+}
