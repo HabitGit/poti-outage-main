a15bccc5b89bb0c76b3ee5f7113f0589bdbe2725
Igor <char.habit@gmail.com>
2023-08-22 11:54:39 +0400

    some fix

--- src/electricity-outage/electricity.service.ts
+++ src/electricity-outage/electricity.service.ts
@@ -66,7 +66,7 @@ export class ElectricityService {
         ...(await this.usersRepository.getUsersByStreetsIdOrNull(null)),
       );
       console.log('[*]FINAL CHATS ID: ', chatsId);
-      // for test
+      // for test123
 
       // Создаем актуальное сообщение
       if (message !== electricityCache) {
