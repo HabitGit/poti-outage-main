310efb2ee10cacfaedee8bc074daf1701cdae9bc
Igor <char.habit@gmail.com>
2023-08-22 17:56:27 +0400

    translater create

---
+++ src/translate/translate.ts
@@ -0,0 +1,61 @@
+import { StreetsRepository } from '../social/repository/streets.repository';
+import { Streets } from '../db/entitys/streets.entity';
+
+const alphabet: Map<string, string> = new Map<string, string>([
+  ['ა', 'a'],
+  ['ბ', 'b'],
+  ['გ', 'g'],
+  ['დ', 'd'],
+  ['ე', 'e'],
+  ['ვ', 'v'],
+  ['ზ', 'z'],
+  ['თ', 't'],
+  ['ი', 'i'],
+  ['კ', 'k'],
+  ['ლ', 'l'],
+  ['მ', 'm'],
+  ['ნ', 'n'],
+  ['ო', 'o'],
+  ['პ', 'p'],
+  ['ჟ', 'zh'],
+  ['რ', 'r'],
+  ['ს', 's'],
+  ['ტ', 't'],
+  ['უ', 'u'],
+  ['ფ', 'p'],
+  ['ქ', 'k'],
+  ['ღ', 'gh'],
+  ['ყ', 'q'],
+  ['შ', 'sh'],
+  ['ჩ', 'ch'],
+  ['ც', 'ts'],
+  ['ძ', 'dz'],
+  ['წ', 'ts'],
+  ['ჭ', 'ch'],
+  ['ხ', 'kh'],
+  ['ჯ', 'j'],
+  ['ჰ', 'h'],
+]);
+
+export class Translate {
+  constructor(private streetsRepository: StreetsRepository) {}
+
+  async translateToRus() {
+    const streets: Streets[] = await this.streetsRepository.getStreets();
+    console.log('str: ', streets);
+    for (const street of streets) {
+      let result: string = '';
+      for (let i = 0; i < street.nameGeo.length; ++i) {
+        if (alphabet.get(street.nameGeo[i]) === undefined) {
+          result += street.nameGeo[i];
+        } else {
+          result += alphabet.get(street.nameGeo[i]);
+        }
+      }
+      await this.streetsRepository.updateStreetById({
+        ...street,
+        nameEng: result,
+      });
+    }
+  }
+}
